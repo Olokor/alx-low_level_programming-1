4f399816f0b95241ac5b7f95cbb290773e64b0d8
Moses Oghene <mosesogheneo@gmail.com>
2022-04-07 18:32:45 -0700

    4-puts.c-fixed-betty-description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-/*
+/**
  *main - entry point
  *
  *Return: 0 after printing the function
  */
 int main(void)
 {
-/*
-*Function:  main
-*--------------------
-*My main function
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
